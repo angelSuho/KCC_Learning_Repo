772a33d9915f6dfa3c7fdf4580a3991bd0be305c
angelSuho <angelsuho0303@gmail.com>
2024-05-29 18:09:16 +0900

    learn/05.29

--- java_work/Kcc_java/src/kosa/oop/Member.java
+++ java_work/Kcc_java/src/kosa/oop/Member.java
@@ -2,17 +2,14 @@ package kosa.oop;
 
 public class Member {
 	// 회원 정보에 대한 클래스와 객체를 생성후 출력해 봅시다.
-	String name;
-	int age;
-	String address;
+	private String name;
+	private int age;
+	private String address;
 	
+	public Member() {}
 	public Member(String name, int age, String address) {
 		this.name = name;
 		this.age = age;
-		this.address = address;
-	}
-	
-	public void updateAddress(String address) {
 		this.address = address;
 	}
 
@@ -20,5 +17,34 @@ public class Member {
 		System.out.println("이름: " + this.name);
 		System.out.println("나이: " + this.age);
 		System.out.println("주소: " + this.address);
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void updateAddress(String address) {
+		this.address = address;
 	}
 }
--- java_work/Kcc_java/src/kosa/oop/MemberMain.java
+++ java_work/Kcc_java/src/kosa/oop/MemberMain.java
@@ -5,11 +5,16 @@ public class MemberMain {
 	public static void main(String[] args) {
 		Member member = new Member("이수호", 26, "서울시 동작구");	// 객체 생성 -> 인스턴스화 : member -> 인스턴스 변수
 		
-		System.out.println(member.address);
+		System.out.println(member.getAddress());
 		
 		member.updateAddress("서울시 어디구");
-		
 		member.printMember();
+		
+		Member member2 = new Member();
+		member2.setName("이수호");
+		member2.setAge(26);
+		member2.setAddress("서울시 짱짱구");
+		member2.printMember();
 	}
 
 }
