7f236773e1f771977d06668a9bccd1f4ec726aa5
angelSuho <angelsuho0303@gmail.com>
2024-05-31 16:58:03 +0900

    learn: 24.05.29/test-oop_inherit

--- java_work/Kcc_java/src/kosa/video/Main.java
+++ java_work/Kcc_java/src/kosa/video/Main.java
@@ -3,17 +3,37 @@ package kosa.video;
 public class Main {
 	
 	public static void main(String[] args) {
-		Video video1 = new Video(1L, "트랜스포머", "서봉수");
-		Video video2 = new Video(2L, "쿵더펜더2", "지성민");
+		Video[] videos = new Video[] {
+				new Video(1L, "트랜스포머", "서봉수"),
+				new Video(2L, "쿵더펜더2", "지성민")
+		};
 		
-		GeneralMember generalMember1 = new GeneralMember("aaa", "홍길동", "동탄");
-		GeneralMember generalMember2 = new GeneralMember("bbb", "김철수", "서울");
+//		GeneralMember generalMember = new GeneralMember("aaa", "홍길동", "동탄");
+//		SpecialMember specialMember = new SpecialMember(10, "bbb", "김철수", "서울");
+//		
+//		generalMember.rental(videos[0]);
+//		specialMember.rental(videos[1]);
+//		
+//		generalMember.printVideos();
+//		System.out.println("-----------------------------");
+//		specialMember.printVideos();
 		
-		generalMember1.rental(video1);
-		generalMember2.rental(video2);
+		// GeneralMember와 SpecialMember를 함께 배열에 넣고 
+		// 각각의  rental(), show() 호출 출력
+		// 특히 SpecialMember는 specialPrint() 호출되도록 하여라.
+		GeneralMember[] members = new GeneralMember[] {
+				new GeneralMember("aaa", "홍길동", "동탄"),
+				new SpecialMember(10, "bbb", "김철수", "서울")
+		};
 		
-		generalMember1.printVideos();
-		System.out.println("-----------------------------");
-		generalMember2.printVideos();
+		for (int i = 0; i < members.length; i++) {
+			members[i].rental(videos[i]);
+			members[i].printVideos();
+			
+			if (members[i] instanceof SpecialMember) {
+				SpecialMember sMember = (SpecialMember) members[1];
+				sMember.specialPrint();
+			}
+		}
 	}
 }
